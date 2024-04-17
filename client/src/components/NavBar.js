68eabca7ad67436273422571200ea4a4518527d6
ChenPingA <chen.ping1@northeastern.edu>
2024-04-17 19:16:45 -0400

    add the components of navbar, one for home, the other is to change the theme of the wholewebsite

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,44 +1,51 @@
-import { AppBar, Container, Toolbar, Typography } from '@mui/material'
+import React from 'react';
+import { AppBar, Container, Toolbar, Typography, IconButton } from '@mui/material';
+import Brightness4Icon from '@mui/icons-material/Brightness4';
 import { NavLink } from 'react-router-dom';
+import { useThemeContext } from './themeContext'; 
 
-// The hyperlinks in the NavBar contain a lot of repeated formatting code so a
-// helper component NavText local to the file is defined to prevent repeated code.
 function NavText({ href, text, isMain }) {
   return (
     <Typography
-      variant={isMain ? 'h5' : 'h7'}
+      variant={isMain ? 'h5' : 'h6'} 
       noWrap
       style={{
-        marginRight: '30px',
-        fontFamily: 'monospace',
-        fontWeight: 700,
-        letterSpacing: '.3rem',
+        marginRight: '30px', 
+        fontFamily: 'monospace', 
+        fontWeight: 700, 
+        letterSpacing: '.3rem', 
       }}
     >
       <NavLink
-        to={href}
+        to={href} 
         style={{
-          color: 'inherit',
-          textDecoration: 'none',
+          color: 'inherit', 
+          textDecoration: 'none', 
         }}
       >
         {text}
       </NavLink>
     </Typography>
-  )
+  );
 }
 
-// Here, we define the NavBar. Note that we heavily leverage MUI components
-// to make the component look nice. Feel free to try changing the formatting
-// props to how it changes the look of the component.
 export default function NavBar() {
+  const { toggleTheme } = useThemeContext(); // 确保这里的名称与你在 Provider 中提供的名称一致
+
   return (
     <AppBar position='static'>
       <Container maxWidth='xl'>
         <Toolbar disableGutters>
-          {/* <NavText href='/' text='SWIFTIFY' isMain />
-          <NavText href='/albums' text='ALBUMS' />
-          <NavText href='/songs' text='SONGS' /> */}
+          <IconButton
+            edge="start"
+            color="inherit"
+            aria-label="toggle theme"
+            onClick={toggleTheme} // 使用解构得到的函数
+            sx={{ mr: 2 }}
+          >
+            <Brightness4Icon />
+          </IconButton>
+          <NavText href='/' text='HOME' isMain /> 
         </Toolbar>
       </Container>
     </AppBar>
