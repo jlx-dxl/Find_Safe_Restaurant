368df888adfc91bdb059b00f3fab25044a1afd82
jlx-dxl <2559346258@qq.com>
2024-04-21 04:26:03 -0400

    backend v1

--- server/server.js
+++ server/server.js
@@ -2,23 +2,29 @@ const express = require('express');
 const cors = require('cors');
 const config = require('./config');
 const routes = require('./routes');
+const swaggerUi = require('swagger-ui-express');
+const swaggerJsdoc = require('swagger-jsdoc');
+const swaggerOptions = require('./swaggerOptions');
 
 const app = express();
 app.use(cors({
   origin: '*',
 }));
 
+// Initialize swagger-jsdoc -> returns validated swagger spec in json format
+const swaggerSpec = swaggerJsdoc(swaggerOptions);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+
 // We use express to define our various API endpoints and
 // provide their handlers that we implemented in routes.js
-app.get('/author/:type', routes.author);
-app.get('/random', routes.random);
-app.get('/song/:song_id', routes.song);
-app.get('/album/:album_id', routes.album);
-app.get('/albums', routes.albums);
-app.get('/album_songs/:album_id', routes.album_songs);
-app.get('/top_songs', routes.top_songs);
-app.get('/top_albums', routes.top_albums);
-app.get('/search_songs', routes.search_songs);
+app.get('/searchRestaurant', routes.searchRestaurant);
+app.get('/getRestaurantInfo', routes.getRestaurantInfo);
+app.get('/getInspectionScore', routes.getInspectionScore);
+app.get('/getCrimeNearRes', routes.getCrimeNearRes);
+app.get('/getDangerScore', routes.getDangerScore);
+app.get('/getRestaurantInspection', routes.getRestaurantInspection);
+app.get('/getNearbyRestaurant', routes.getNearbyRestaurant);
 
 app.listen(config.server_port, () => {
   console.log(`Server running at http://${config.server_host}:${config.server_port}/`)
---
+++ server/swaggerOptions.js
@@ -0,0 +1,32 @@
+// swaggerOptions.js
+const swaggerDefinition = {
+    openapi: '3.0.0',
+    info: {
+      title: 'FOOD_CRIME_CHICAGO  API Documentation',
+      version: '1.0.0',
+      description: 'This is the swagger doc for the CIS5500 Final Proj',
+      license: {
+        name: 'Licensed Under MIT',
+        url: 'https://spdx.org/licenses/MIT.html',
+      },
+      // contact: {
+      //   name: 'Your Name',
+      //   url: 'Your Website',
+      // },
+    },
+    servers: [
+      {
+        url: 'http://localhost:8080',
+        description: 'Development server',
+      },
+    ],
+  };
+  
+  const options = {
+    swaggerDefinition,
+    // Paths to files containing OpenAPI definitions
+    apis: ['./routes.js'],
+  };
+  
+  module.exports = options;
+  
