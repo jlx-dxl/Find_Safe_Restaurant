07d3260d2bf7f08646f45f0ed84fe92d5319b618
Xinling Deng <cindy-deng@foxmail.com>
2024-05-06 22:23:24 -0400

    finish deploy config

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const cors = require('cors');
 const config = require('./config');
 const routes = require('./routes');
@@ -15,6 +16,8 @@ app.use(cors({
 const swaggerSpec = swaggerJsdoc(swaggerOptions);
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
+// 提供静态文件
+app.use(express.static(path.join(__dirname, '../client/build')));
 
 // We use express to define our various API endpoints and
 // provide their handlers that we implemented in routes.js
@@ -29,9 +32,20 @@ app.get('/getRestaurantOverallScore', routes.getRestaurantOverallScore);
 app.get('/getCrimeRankByID', routes.getCrimeRankByID);
 app.get('/getSafetyScore', routes.getSafetyScore);
 
+// 所有路由指向index.html
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+});
+
+
+const PORT = process.env.PORT || config.server_port;
+const HOST = '0.0.0.0';
+
 if(process.env.NODE_ENV !== 'test') {
-  console.log("running")
-  app.listen(config.server_port, () => {
+  // app.listen(config.server_port, () => {
+  //   console.log(`Server running at http://${config.server_host}:${config.server_port}/`)
+  // });
+  app.listen(PORT, HOST, () => {
     console.log(`Server running at http://${config.server_host}:${config.server_port}/`)
   });
 }
